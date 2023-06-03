dce705bbefd3ce8fe698059310fb6b8a6429f362
Prashul Jain <jainprashul@gmail.com>
2023-06-03 20:10:45 +0530

    Cron Job added.

--- dist/index.js
+++ dist/index.js
@@ -13,29 +13,45 @@ const configuration = new openai_1.Configuration({
 });
 const openai = new openai_1.OpenAIApi(configuration);
 const T = new twitter_api_v2_1.TwitterApi({
-    appKey: process.env.TWITTER_API_KEY ?? '',
-    appSecret: process.env.TWITTER_KEY_SECERT ?? '',
-    accessToken: process.env.T_ACCESS_TOKEN ?? '',
-    accessSecret: process.env.T_ACCESS_TOKEN_SECERT ?? ''
+    appKey: process.env.TWITTER_API_KEY ?? "",
+    appSecret: process.env.TWITTER_KEY_SECERT ?? "",
+    accessToken: process.env.T_ACCESS_TOKEN ?? "",
+    accessSecret: process.env.T_ACCESS_TOKEN_SECERT ?? "",
 });
+let msgs = [];
 async function askGPT(prompt) {
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{
+        messages: [
+            {
                 role: "system",
-                content: 'You are Tech Blogger , You specialize in Full Stack Development. You should give tip of the day for the technologies such as Javascript , Typescript, UI design. Give the concise summary in max 250 words of the any of these above mentioned technology to show daily tweet. Give a one right now. '
-            },]
+                content: "You are Tech Blogger , You specialize in Full Stack Development. You should give tip of the day for the technologies such as Javascript , Typescript, UI design, Nodejs, React, Docker, CSS, HTML, Kubernetes, system design , git , python . Give the concise summary for any of these above mentioned technology to show daily tweet. Make sure to keep the tweet short upto 280 characters.",
+            },
+            ...msgs,
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
     });
-    const text = completion.data.choices.map(c => c.message?.content).join("\n");
+    const text = completion.data.choices.map((c) => c.message?.content).join("\n");
     console.log(text);
-    const tweet = await T.v2.tweet(text);
-    console.log(tweet);
+    msgs.push({
+        role: completion.data.choices[0].message?.role ?? "assistant",
+        content: text,
+    });
     return text;
 }
-;
-// every 12 hours run the function
-node_cron_1.default.schedule('*/10 * * * *', () => {
-    console.log(new Date().toLocaleString());
+async function tweet(text) {
+    await T.v2.tweet(text);
+}
+async function autoTweet() {
     console.log("Tweetting....");
-    askGPT("Give a one right now.");
+    const str = await askGPT("Give a one right now. Make sure no duplicate tweets and confirmation.");
+    await tweet(str);
+    console.log("Tweeted....");
+}
+node_cron_1.default.schedule("*/1 * * * *", () => {
+    console.log(new Date().toLocaleString());
+    autoTweet();
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import "dotenv/config"
+import "dotenv/config";
 import { Configuration, OpenAIApi } from "openai";
 import { TwitterApi } from "twitter-api-v2";
-import cron from 'node-cron'
+import cron from "node-cron";
 // get the open ai function get the text based on the prompt
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,35 +9,55 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const T = new TwitterApi({
-  appKey : process.env.TWITTER_API_KEY ?? '',
-  appSecret : process.env.TWITTER_KEY_SECERT ?? '',
-  accessToken : process.env.T_ACCESS_TOKEN ?? '',
-  accessSecret : process.env.T_ACCESS_TOKEN_SECERT ?? ''
-})
+  appKey: process.env.TWITTER_API_KEY ?? "",
+  appSecret: process.env.TWITTER_KEY_SECERT ?? "",
+  accessToken: process.env.T_ACCESS_TOKEN ?? "",
+  accessSecret: process.env.T_ACCESS_TOKEN_SECERT ?? "",
+});
+
+let msgs: {
+  role: "assistant" | "user" | "system";
+  content: string;
+}[] = [];
 
 async function askGPT(prompt: string) {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages : [{
-      role : "system",
-      content : 'You are Tech Blogger , You specialize in Full Stack Development. You should give tip of the day for the technologies such as Javascript , Typescript, UI design. Give the concise summary in max 250 words of the any of these above mentioned technology to show daily tweet. Give a one right now. '
-    }, ]
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are Tech Blogger , You specialize in Full Stack Development. You should give tip of the day for the technologies such as Javascript , Typescript, UI design, Nodejs, React, Docker, CSS, HTML, Kubernetes, system design , git , python . Give the concise summary for any of these above mentioned technology to show daily tweet. Make sure to keep the tweet short upto 280 characters.",
+      },
+      ...msgs,
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
   });
 
-  const text = completion.data.choices.map(c => c.message?.content).join("\n")
-  console.log(text)
+  const text = completion.data.choices.map((c) => c.message?.content).join("\n");
+  console.log(text);
+  msgs.push({
+    role: completion.data.choices[0].message?.role ?? "assistant",
+    content: text,
+  });
+  return text;
+}
 
-  const tweet = await T.v2.tweet(text)
-  console.log(tweet)
+async function tweet(text: string) {
+  await T.v2.tweet(text);
+}
 
-  return text
-};
+async function autoTweet() {
+  console.log("Tweetting....");
+  const str = await askGPT("Give a one right now.");
+  await tweet(str);
+  console.log("Tweeted....");
+}
 
-
-// every 12 hours run the function
-
-cron.schedule('*/10 * * * *', () => {
-  console.log(new Date().toLocaleString())
-  console.log("Tweetting....")
-  askGPT("Give a one right now.")
-})
+cron.schedule("0 12,17 * * *", () => {
+  console.log(new Date().toLocaleString());
+  autoTweet();
+});
